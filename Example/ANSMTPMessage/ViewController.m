2d52ccf8d4eea965f3ce698ffca1c5a74fbcdf06
anselPeng <834893274@qq.com>
2018-07-31 16:17:29 +0800

    fix bug

--- Example/ANSMTPMessage/ViewController.m
+++ Example/ANSMTPMessage/ViewController.m
@@ -17,26 +17,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-
 }
-
-
-
 - (IBAction)sendMailFromQQ:(id)sender {
     [self mailFromQQ];
 }
-
 
 - (IBAction)sendMainFrom163:(id)sender {
     [self mailFrom163];
 }
 
-
 - (void)mailFromQQ{
     [ANSMTPMessageManager setupConfigWithServer:@"smtp.qq.com" withFrom:@"你的qq邮箱@qq.com" withPassword:@"授权码"];
-    
-    
     NSString * sb = [NSString stringWithFormat:@"%ld",random()%100000];
     NSString * body = [NSString stringWithFormat:@"%ld",random()%100000];
     NSArray * cc = @[@"xxx@163.com",@"xxx@csc-sz.com"];
@@ -47,31 +38,20 @@
 - (void)mailFrom163{
     
     [ANSMTPMessageManager setupConfigWithServer:@"smtp.163.com" withFrom:@"xxxxx@163.com" withPassword:@"xxxxx"];
-    
-    
     NSString * sb = [NSString stringWithFormat:@"%ld",random()%100000];
     NSString * body = [NSString stringWithFormat:@"%ld",random()%100000];
     NSArray * cc = @[@"xxxxxxx@csc-sz.com"];
     NSString * path =[[NSBundle mainBundle] pathForResource:@"ex.txt" ofType:nil];
-    
     [ANSMTPMessageManager sendMaileWithTo:@"xxxxxx@qq.com" cc:cc subject:sb body:body path:path delegate:self];
 }
 
-
 - (void)messageSent:(ANSMTPMessage *)message{
-    
     NSLog(@"邮件发送成功了");
 }
 
-- (void)messageFailed:(ANSMTPMessage *)message error:(NSError *)error{
-    
+- (void)messageFailed:(ANSMTPMessage *)message error:(NSError *)error{ 
     NSLog(@"邮件发送失败了 ：%@",error);
-  
 }
-
-
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
